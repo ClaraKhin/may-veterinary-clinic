e4f4acfc674edada42e007a16c79965279176889
ClaraKhin <khinthirikyaw8@gmail.com>
2023-11-01 22:55:24 +0630

    Update EditModal.jsx

--- src/pages/components/EditModal.jsx
+++ src/pages/components/EditModal.jsx
@@ -1,39 +1,44 @@
-import React from "react";
 import styles from "../../styles/EditModal.module.css";
 
 const EditModal = (props) => {
   return (
     <div>
       <form onSubmit={props.handleEditFormSubmit} className={styles.editForm}>
-        {props.filterColumn &&
-          props.filterColumn.map((col) => (
-            <div key={col.field}>
-              <label htmlFor={col.field} className={styles.label}>
-                {col.headerName}:{" "}
-              </label>
-              {col.field === "id" ? (
-                <input
-                  type="text"
-                  id={col.field}
-                  name={col.field}
-                  className={styles.inputField}
-                  value={props.selectedRow[col.field]}
-                  readOnly
-                />
-              ) : (
-                <input
-                  type="text"
-                  id={col.field}
-                  name={col.field}
-                  className={styles.inputField}
-                  value={props.selectedRow[col.field]}
-                  onChange={(e) =>
-                    props.handleEditInputChange(col.field, e.target.value)
-                  }
-                />
-              )}
-            </div>
-          ))}
+        <div className="text-center m-3">
+          <h3 className={styles.editTitle}>Update patient</h3>
+          <h4 className={styles.editSubTitle}>
+            Enter update patient information below
+          </h4>
+        </div>
+        <div className={styles.formInputSec}>
+          {props.filterColumn &&
+            props.filterColumn.map((col) => (
+              <div key={col.field} className={styles.editSection}>
+                {col.field === "id" ? (
+                  <input
+                    type="text"
+                    id={col.field}
+                    name={col.field}
+                    className={styles.inputField}
+                    value={props.selectedRow[col.field]}
+                    readOnly
+                  />
+                ) : (
+                  <input
+                    type="text"
+                    id={col.field}
+                    name={col.field}
+                    className={styles.inputField}
+                    value={props.selectedRow[col.field]}
+                    onChange={(e) =>
+                      props.handleEditInputChange(col.field, e.target.value)
+                    }
+                  />
+                )}
+              </div>
+            ))}
+        </div>
+
         <div className={styles.editBtns}>
           <button
             type="submit"
